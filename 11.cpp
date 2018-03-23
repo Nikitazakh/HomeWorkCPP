6037dff6ae049b74a056d042c9c8d51eeddeda0a
Nikitazakh <nikitazakh@gmail.com>
2018-03-23 22:33:31 +0300

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -37,7 +37,28 @@ std::list <T> parallel_quick_sort(std::list <T> input)
     return result;
 }
 
+template <typename T>
+std::list <T> quick_sort(std::list <T> input)
+{
+    if (input.empty())
+    {
+        return input;
+    }
+    std::list <T> result;
+    result.splice(result.begin(), input, input.begin());
+    const T & ref_point = *result.begin();
+    auto bound = std::partition(input.begin(), input.end(), [&](const T & t){return t<ref_point;});
 
+    std::list <T> any_list;
+    any_list.splice(any_list.begin(),input, input.begin(), bound);
+
+    std::list<T> any_future = quick_sort(any_list);
+    std::list<T> new_list_high = quick_sort(input);
+
+    result.splice(result.end(), new_list_high);
+    result.splice(result.begin(), any_future);
+    return result;
+}
 
 
 int main(int argc, char ** argv)
@@ -56,18 +77,18 @@ int main(int argc, char ** argv)
 
     std::chrono::time_point<std::chrono::system_clock> start_par, end_par;
     start_par = std::chrono::system_clock::now();
-    std::list<int> res = parallel_quick_sort(need_to_sort);
+    std::list<int> res_par = parallel_quick_sort(need_to_sort);
     end_par = std::chrono::system_clock::now();
 
     std::chrono::time_point<std::chrono::system_clock> start_q, end_q;
     start_q = std::chrono::system_clock::now();
-    need_to_sort.sort();
+    std::list<int> res_q = quick_sort(need_to_sort);
     end_q = std::chrono::system_clock::now();
 
-    int elapsed_seconds_par = std::chrono::duration_cast<std::chrono::nanoseconds>(end_par-start_par).count();
-    int elapsed_seconds_q = std::chrono::duration_cast<std::chrono::nanoseconds>(end_q-start_q).count();
+    int elapsed_seconds_par = std::chrono::duration_cast<std::chrono::microseconds>(end_par-start_par).count();
+    int elapsed_seconds_q = std::chrono::duration_cast<std::chrono::microseconds>(end_q-start_q).count();
 
-    std::cout << "Time for parallel: " << elapsed_seconds_par << " ns\n";
-    std::cout << "Time for quik: " << elapsed_seconds_q << " ns\n";
+    std::cout << "Time for parallel: " << elapsed_seconds_par << " mks\n";
+    std::cout << "Time for quick: " << elapsed_seconds_q << " mks\n";
 
 }
